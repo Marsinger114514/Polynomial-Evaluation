a6a5a60cd42ef61390513e0b15852ac53d2b5d37
Marsinger114514 <2640224286@qq.com>
2024-11-26 23:57:19 +0800

    fourth commit

--- TestPolynomial.cpp
+++ TestPolynomial.cpp
@@ -14,8 +14,9 @@ int main()
 		cout  <<"5. 多项式乘法." << endl;
 		cout << "6. 多项式减法." << endl;
 		cout << "7. 多项式求值." << endl;
+		cout << "8. 综合运算." << endl;
 		cout << "0. 退出." << endl;
-		cout  <<"选择功能(0~7):" << endl;
+		cout  <<"选择功能(0~8):" << endl;
 		cin >> c;
 		switch (c) {
 		case '1':
@@ -61,6 +62,7 @@ int main()
 			fc.Display();
 			cout << endl;
 			break;
+			fa = fc;
 		case '4':
 			fa.SetZero();
 			cout << endl << "输入(coef, expn)(expn = -1时退出):" << endl;
@@ -74,10 +76,11 @@ int main()
 			fa.Display();
 			cout << endl;
 
-			cout << "fb:" << endl;			// 显示fa
+			cout << "d(fa):" << endl;			// 显示fa
 			fb.Display();
 			cout << endl;
 			break;
+			fa = fb;
 		case '5':
 			fa.SetZero();		// 将fa设置为0
 			cout << endl << "输入fa的项(coef, expn)(e = -1时退出):" << endl;
@@ -108,7 +111,7 @@ int main()
 			fc.Display();
 			cout << endl;
 			break;
-
+			fa = fc;
 		case '6':
 			// 输入fa
 			fa.SetZero();		// 将fa设置为0
@@ -141,13 +144,38 @@ int main()
 			fc.Display();
 			cout << endl;
 			break;
-
+			fa = fc;
 		case '7':
 			cout << endl << "输入要计算的多项式的x值: ";
 			int x;
 			cin >> x;
 			cout << "多项式在x = " << x << "处的值为: " << fa.Evaluate(x) << endl;
 			break;
+
+		case '8':
+			// 输入fa
+			fa.SetZero();		// 将fa设置为0
+			cout << endl << "输入fa的项(coef, expn)(e = -1时退出):" << endl;
+			cin >> it.coef >> it.expn;
+			while (it.expn != -1) {
+				fa.InsItem(it);
+				cin >> it.coef >> it.expn;
+			}
+
+			// 输入fb
+			fb.SetZero();		// 将fb设置为0
+			cout << endl << "输入fb的项(coef, expn)(e = -1时退出):" << endl;
+			cin >> it.coef >> it.expn;
+			while (it.expn != -1) {
+				fb.InsItem(it);
+				cin >> it.coef >> it.expn;
+			}
+
+			fc = fa + fb + fa.d() + fb.d();
+			cout << "fa+fb+d(fa)+d(fb):" << endl;			// 显示fa
+			fc.Display();
+			cout << endl;
+			fa = fc;
 		}
 	}
 	system("PAUSE");				// 调用库函数system()
